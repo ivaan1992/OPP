40f7d5b8de19dc6e150824f812ca3b5b4ff27a17
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-06-15 16:49:36 -0500

    Fix linters issues

--- base_decorator.rb
+++ base_decorator.rb
@@ -1,12 +1,12 @@
 require_relative 'nameable'
 
 class Decorator < Nameable
-    def initialize(nameable)
-        super()
-        @nameable = nameable
-    end
-    
-    def correct_name
-        @nameable
-    end
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable
+  end
 end
--- person.rb
+++ person.rb
@@ -2,9 +2,9 @@ require './nameable'
 require './capitalizer'
 require './trimmer'
 
-
 class Person < Nameable
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1...1000)
     @name = name
     @age = age
