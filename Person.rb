e914b78e98ef89ef94f929579456dda2e6024f3f
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-06-15 12:28:02 -0500

    Install rubocop gem

--- Person.rb
+++ Person.rb
@@ -1,21 +1,21 @@
-class  Person
-    def initialize(age, id = 0, name = 'unknown', parent_permission: true)
-      @id = id
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
+class Person
+  def initialize(age, id = 0, name = 'unknown', parent_permission: true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    attr_reader :id
-    attr_accessor :age, :name
-    
-    def can_use_services?
-        of_age || @parent_permission
-    end
+  attr_reader :id
+  attr_accessor :age, :name
 
-    private
+  def can_use_services?
+    of_age || @parent_permission
+  end
 
-    def of_age?
-        @age > 18
-    end
+  private
+
+  def of_age?
+    @age > 18
+  end
 end
--- Student.rb
+++ Student.rb
@@ -1,12 +1,12 @@
 require_relative 'Person'
 
 class Student < Person
-    def initialize(age, classroom, name = 'unknown', parent_permission: true)
+  def initialize(age, classroom, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
-    end
+  end
 
-    def play_hooky
-        '¯\(ツ)/¯'
-    end
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
 end
